74e5fe278d88228abc01a5dfe36670fa45478b93
James Jackson <james.ogge@gmail.com>
2023-08-23 07:48:57 +0100

    add advert

--- bday-app/src/components/deal.tsx
+++ bday-app/src/components/deal.tsx
@@ -1,11 +1,13 @@
 import styles from "./deal.module.scss";
 import xboxPicture from "../assets/xbox.webp";
+import backrooms from "../assets/backrooms.png";
 const Deal = (): JSX.Element => {
 	return (
 		<div className={[styles.deal].join(" ")} data-cy="Deal">
-			<img src={xboxPicture} />
-			<h2>Up to 20% off XBox Series S</h2>
+			<img src={xboxPicture} className={styles.deal__bgImg} />
+			<h2>Up to 10% off XBox Series S</h2>
 			<p>Grab yours now, or you are a biscuit.</p>
+			<img src={backrooms} className={styles.deal__ad} />
 		</div>
 	);
 };
